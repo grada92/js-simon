7adb89375876c945144c0de754a3307754086b08
grada92 <danielegradassai@gmail.com>
2022-09-05 19:40:26 +0200

    condizione indovina o sbaglia

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ da indovinare sono stati individuati.
 */
 
 console.log('JS OK')
-
+// Collego listnumber a Html
 const listnumbers = document.getElementById('Number');
 
 const array = [];
-
+// Creo ciclo while 
 while (array.length < 5) {
     
     const numRandom = Math.floor(Math.random() * 100 + 1);
@@ -26,11 +26,46 @@ while (array.length < 5) {
 }
 
 console.log(array);
-
+// inserisco valore array dentro html
 listnumbers.innerHTML = array;
 
 const reset = setTimeout(function() {
 
-    listaNumeri.innerHTML = '';
+    listnumbers.innerHTML = '';
 
 }, 3000 );
+
+const userArray = [];
+
+const emptyBox = setTimeout(function() {
+// Creo ciclo while numeri utente
+while (userArray.length < 5) {     
+
+const userNumbers = parseInt(prompt('Inserisci i numeri che ricordi'));
+
+userArray.push(userNumbers);
+
+}
+
+
+
+// Creo condizione per cui utente indovina o sbaglia
+for (let i = 0; i < userArray.length; i++) {
+
+if (userArray[i] === array[i]) {
+    console.log("Hai indovinato!");
+    console.log(`
+Numero random ${array[i]}, Numero inserito ${userArray[i]}
+            `);
+} else {
+    console.log("Ritenta!");
+    console.log(`
+    Numero random ${array[i]}, Numero inserito ${userArray[i]}
+    `);
+
+}
+}
+
+}, 3500 );
+
+console.log(userArray);
